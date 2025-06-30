ea688a070c18649118c735ed2cdc4df8a358eb79
Everestfin1 <contact@everestfin.com>
2025-06-30 17:40:12 +0000

    Ramener la landing page

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MaintenancePage } from "./screens/MaintenancePage";
+//import { MaintenancePage } from "./screens/MaintenancePage";
+import { LandingPage } from "./screens/LandingPage";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <MaintenancePage />
+    {/*<MaintenancePage />*/}
+    <LandingPage />
   </StrictMode>,
 );
