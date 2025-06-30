31f78431858c87708e2b723fca0959d8b84f9707
Everestfin1 <contact@everestfin.com>
2025-06-30 17:53:02 +0000

    Changement dev : ramener la LP

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MaintenancePage } from "./screens/MaintenancePage";
-//import { LandingPage } from "./screens/LandingPage";
+//import { MaintenancePage } from "./screens/MaintenancePage";
+import { LandingPage } from "./screens/LandingPage";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <MaintenancePage />
-    {/*<LandingPage />*/}
+    {/*<MaintenancePage />*/}
+    <LandingPage />
   </StrictMode>,
 );
