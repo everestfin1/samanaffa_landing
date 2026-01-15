c4d7d9214968fd9e7d713c512c4e0317949b51f2
aliouuuw <wadealiou00@gmail.com>
2026-01-15 15:51:07 +0000

    feat: add Facebook Pixel tracking with ID 2209795136215271
    - Add Facebook Pixel script with afterInteractive strategy
    - Initialize pixel with ID 2209795136215271 and PageView tracking
    - Add noscript fallback image for non-JS environments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,16 @@ export default function RootLayout({
             window.gtag('config', 'G-SXLJ8F6H1L');
           `}
         </Script>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2209795136215271');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" src="https://www.facebook.com/tr?id=2209795136215271&ev=PageView&noscript=1"/>
+        </noscript>
       </head>
       <body className={`${geistSans.variable} antialiased`}>
         <QueryProvider>
